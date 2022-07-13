e6219a2ba763eb02509140eb0007039b45570d90
email <mmb2020max@gmail.com>
2022-07-13 14:56:41 +0200

    Debugged with Breakpoint

--- property_decorator.py
+++ property_decorator.py
@@ -15,32 +15,32 @@ A property object has three methods, getter(), setter(), and deleter() to specif
 # Using @property decorator
 class Celsius:
     def __init__(self, temperature=0):
-        self.temperature = temperature
+        self.temperature = temperature  #2    #6       #19   #23
 
     def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
+        return (self.temperature * 1.8) + 32    #13   # 16
 
     @property
     def temperature(self):
-        print("Getting value...")
-        return self._temperature
+        print("Getting value...") #9 prints      #14 Prints
+        return self._temperature   #10           #15
 
     @temperature.setter
     def temperature(self, value):
-        print("Setting value...")
-        if value < -273.15:
-            raise ValueError("Temperature below -273 is not possible")
-        self._temperature = value
+        print("Setting value...")  #3 prints in terminal     #20
+        if value < -273.15:  #4        # 21
+            raise ValueError("Temperature below -273 is not possible")   # 22  # 25 ERROR :Temperature below -273 is not possible
+        self._temperature = value   #5
 
 
 # create an object
-human = Celsius(37)
+human = Celsius(37)    #1 Breakpoint    #7
 
-print(human.temperature)
+print(human.temperature) #8   #11
 
-print(human.to_fahrenheit())
+print(human.to_fahrenheit())    #12   # 17
 
-coldest_thing = Celsius(-300)
+coldest_thing = Celsius(-300)  # 18  #24
 
 
 # Output:
@@ -50,3 +50,4 @@ coldest_thing = Celsius(-300)
     # Getting value...
     # 98.60000000000001
     # Setting value...
+    #ERROR:Temperature below -273 is not possible
